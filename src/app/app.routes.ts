1786d6db84396f1711816851a27992718202e890
Rifat2022 <md.rifathossainshaon@gmail.com>
2024-08-21 11:44:32 +0600

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Home/home/home.component';
+import { PageNotFoundComponent } from './NotFound/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'index', component: HomeComponent },
+    { path: '', redirectTo: '/index', pathMatch: 'full' }, // Default route
+    { path: '**', component: PageNotFoundComponent }  // Wildcard route for a 404 page
+];
