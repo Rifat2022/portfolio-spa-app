0525b1ad4ffa315deba208a8e4b657d0c441fef6
Rifat2022 <md.rifathossainshaon@gmail.com>
2024-08-23 12:18:32 +0600

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Home/home/home.component';
 import { PageNotFoundComponent } from './NotFound/page-not-found/page-not-found.component';
+import { PortfolioDetailsComponent } from './Portfolio-Details/portfolio-details/portfolio-details.component';
+import { SingleBlogComponent } from './Blog/single-blog/single-blog.component';
 
 export const routes: Routes = [
     { path: 'index', component: HomeComponent },
+    { path: 'portfolio-details', component: PortfolioDetailsComponent },
+    { path: 'blog-single', component: SingleBlogComponent },
     { path: '', redirectTo: '/index', pathMatch: 'full' }, // Default route
     { path: '**', component: PageNotFoundComponent }  // Wildcard route for a 404 page
 ];
