916f904d4584457df7abe73258e7f09cbdeb52b1
Rifat2022 <md.rifathossainshaon@gmail.com>
2024-09-29 13:28:29 +0600

    update

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -1,17 +1,16 @@
 import { CommonModule, isPlatformBrowser } from "@angular/common";
 import { 
   Component, 
-  Input, 
   OnInit, 
   CUSTOM_ELEMENTS_SCHEMA, 
-  ContentChild, 
   ElementRef, 
-  effect, 
   AfterViewInit, 
   ViewChild, 
-  ViewContainerRef,
-   PLATFORM_ID,
-   Inject, 
+  PLATFORM_ID,
+  Inject,
+  HostListener,
+  Renderer2,
+  OnDestroy, 
 } from "@angular/core";
 import { ContactComponent } from "../Contact/contact.component";
 import { NavLink } from "../Models/navLinks.model";
@@ -21,8 +20,8 @@ import { Portfolio } from "../Models/portfolio.model";
 import { Testimonial } from "../Models/testimonial.model";
 import { BlogPost } from "../Models/blog-post.model";
 import { DataService } from "../Services/data.service";
-import { SwiperContainer } from 'swiper/element/bundle';
-
+import Typed from 'typed.js';
+import GLightbox from 'glightbox';
 
 @Component({
   selector: 'app-home',
@@ -33,7 +32,7 @@ import { SwiperContainer } from 'swiper/element/bundle';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   host: {ngSkipHydration: 'true'},
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   navLinks!: NavLink[] 
   services!: Services[] 
   counters!: Counter[] 
@@ -41,37 +40,177 @@ export class HomeComponent implements OnInit, AfterViewInit {
   testimonials!: Testimonial[] 
   blogPosts!: BlogPost[] 
   @ViewChild("testimonialSwiperContainer", {static: false}) testimonialSwiperContainerRef!: ElementRef;
-
-  constructor(private dataService: DataService, @Inject(PLATFORM_ID) private platformId: any) {}
+  @ViewChild("header", {static: false}) headerElemRef!: ElementRef;
+  @ViewChild("backToTop", {static: false}) backToTopRef!: ElementRef;
+  private typed: Typed | null = null;
+  constructor(
+    private dataService: DataService, 
+    @Inject(PLATFORM_ID) private platformId: any, 
+    private renderer2: Renderer2,
+    private elRef: ElementRef
+  ) {}
+  
 
   ngOnInit(): void {
-    this.setDataToComponent(JSON.parse(JSON.stringify(this.dataService.getData())));          
+    var me = this; 
+    me.setDataToComponent(JSON.parse(JSON.stringify(this.dataService.getData())));          
   }
 
   ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.setHomeComponentEvents();
+    var me = this; 
+    if (isPlatformBrowser(me.platformId)) {
+      me.setHomeComponentEvents();
     }
   }
-  private setDataToComponent(Data: any){
-    this.navLinks= Data.navLinks ; 
-    this.services= Data.services ; 
-    this.counters= Data.counters ; 
-    this.workItems= Data.workItems ; 
-    this.testimonials= Data.testimonials ; 
-    this.blogPosts= Data.blogPosts ;
+  ngOnDestroy(): void {
+    var me = this; 
+    me.stop(); 
   }
-  private triggerslideNext(container: any){
-    if(container){
-      setInterval(()=> {
-        container.swiper.slideNext(); 
-      },5000); 
-    }else{
-      throw new Error("swiper-container error!"); 
-    }
+  @HostListener('window:scroll', []) onWindowScroll() {
+    var me = this; 
+    me.toggleHeaderScrolled();
+    me.toggleBackToTopActiveClass();
   }
+
   private setHomeComponentEvents() {
-    this.triggerslideNext(this.testimonialSwiperContainerRef.nativeElement);    
-  }  
+    var me = this; 
+    me.triggerslideNext();
+    me.setScrollToHashElement();
+    me.scrollToHeaderElementFromTopButton();
+    me.startIntroTypedEffect();
+    me.initializeGlightBox();
+  }
+  private initializeGlightBox(){
+    const GLightbox = require('glightbox'); // Loads GLightbox only in the browser
+    const lightbox = GLightbox({
+      selector: '.glightbox'
+    });
+  }
+  private start(element: HTMLElement, options:  any) {
+    var me = this; 
+    me.typed = new Typed(element, options);
+  }
+
+  private stop() {
+    var me = this; 
+    if (me.typed) {
+      me.typed.destroy();
+      me.typed = null;
+    }
+  }
+  private startIntroTypedEffect(){
+    var me = this;
+    const typedElement = me.elRef.nativeElement.querySelector('.typed');
+    if (typedElement) {
+
+      let typed_strings = "Web Developer , Freelancer, Entrepreneur, Blogger"; 
+      // let typed_strings = typedElement.getAttribute('data-typed-items')
+      const role:string[] = (typed_strings) ? typed_strings.split(',') : [];
+      if(role.length > 0){
+        const options: any  = { 
+          strings: role,
+          loop: true,
+          typeSpeed: 120,
+          backSpeed: 70,
+          backDelay: 2500
+        }
+        me.start(typedElement, options); 
+      }
+    }
+  }
+  private  scrollToHeaderElementFromTopButton(){
+    var me = this;
+    const backToTopElementRef : HTMLElement = me.backToTopRef.nativeElement as HTMLElement;
+    me.renderer2.listen(backToTopElementRef, "click", ()=> {
+      me.scrollToHeaderElement(backToTopElementRef)
+    })
+  }
+  private  toggleBackToTopActiveClass(){
+    var me = this; 
+    const backToTop = me.backToTopRef.nativeElement;
+    if (window.scrollY > 100) {
+      backToTop.classList.add('active')
+    } else {
+      backToTop.classList.remove('active')
+    }
+  }
+  private toggleHeaderScrolled() {
+    var me = this; 
+    const header = me.headerElemRef.nativeElement;
+    if (window.scrollY > 100) {
+      header.classList.add('header-scrolled');
+    } else {
+      header.classList.remove('header-scrolled');
+    }
+  }
+  private scrollToHeaderElement(el : HTMLElement ): void {
+    // const header = document.querySelector('#header') as HTMLElement;
+    var me  = this; 
+    const header = me.headerElemRef.nativeElement as HTMLElement;
+    let offset = header.offsetHeight;
+  
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16;
+    }
+  
+    // const elementPos = element.offsetTop;
+  
+    window.scrollTo({
+      top: el.offsetTop - offset,
+      behavior: 'smooth'
+    });
+  }
+  private on(type: string, el: string, callback: any, 
+    all: boolean = false): void {
+      var me =this; 
+      let selectEl!: any ;
+      if (all) {
+        selectEl= me.renderer2.selectRootElement(el, true).querySelectorAll(el) as NodeListOf<Element>
+        selectEl.forEach((e: Element) => me.renderer2.listen(e, type, callback));
+      } else {
+        selectEl= me.renderer2.selectRootElement(el, true).querySelector(el) as Element;
+        me.renderer2.listen(selectEl, type, callback);
+      }
+    
+  }
+  
+  
+  private setDataToComponent(Data: any){
+    var me = this; 
+    me.navLinks= Data.navLinks ; 
+    me.services= Data.services ; 
+    me.counters= Data.counters ; 
+    me.workItems= Data.workItems ; 
+    me.testimonials= Data.testimonials ; 
+    me.blogPosts= Data.blogPosts ;
+  }
+  private triggerslideNext(){
+    var me = this; 
+    if( me.testimonialSwiperContainerRef.nativeElement){
+      setInterval(()=> { me.testimonialSwiperContainerRef.nativeElement.swiper.slideNext()},5000); 
+    }
+  }
+
+  private setScrollToHashElement(){
+    var me = this; 
+    const hash = window.location.hash; // Still use window to get the hash
+    if (hash) {
+      const elementId = hash.substring(1); // Remove the '#' character
+      const element = me.renderer2.selectRootElement(`#${elementId}`, true); // Use Renderer2
+      if (element) me.scrollToHeaderElement(element);      
+    }
+  }
+  private addClassToElements(elementRef:any[], cssClassName: string){
+    elementRef.forEach(element => {
+      element.classList.classList.add(cssClassName)
+    });
+  }
+  private removeClassFromElements(elementRef:any[], cssClassName: string){
+    elementRef.forEach(element => {
+      element.classList.remove(cssClassName)
+    });
+    
+  }
+  
   
 }
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,31 +10,31 @@
   /**
    * Easy selector helper function
    */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+  // const select = (el, all = false) => {
+  //   el = el.trim()
+  //   if (all) {
+  //     return [...document.querySelectorAll(el)]
+  //   } else {
+  //     return document.querySelector(el)
+  //   }
+  // }
 
   /**
    * Easy event listener function
    */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
+  // const on = (type, el, listener, all = false) => {
+  //   let selectEl = select(el, all)
+  //   if (selectEl) {
+  //     if (all) {
+  //       selectEl.forEach(e => e.addEventListener(type, listener))
+  //     } else {
+  //       selectEl.addEventListener(type, listener)
+  //     }
+  //   }
+  // }
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
@@ -63,20 +63,20 @@
   /**
    * Scrolls to an element with header offset
    */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
+  // const scrollto = (el) => {
+  //   let header = select('#header')
+  //   let offset = header.offsetHeight
 
-    if (!header.classList.contains('header-scrolled')) {
-      offset -= 16
-    }
+  //   if (!header.classList.contains('header-scrolled')) {
+  //     offset -= 16
+  //   }
 
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
+  //   let elementPos = select(el).offsetTop
+  //   window.scrollTo({
+  //     top: elementPos - offset,
+  //     behavior: 'smooth'
+  //   })
+  // }
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
@@ -161,25 +161,25 @@
   /**
    * Intro type effect
    */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  // const typed = select('.typed')
+  // if (typed) {
+  //   let typed_strings = typed.getAttribute('data-typed-items')
+  //   typed_strings = typed_strings.split(',')
+  //   new Typed('.typed', {
+  //     strings: typed_strings,
+  //     loop: true,
+  //     typeSpeed: 100,
+  //     backSpeed: 50,
+  //     backDelay: 2000
+  //   });
+  // }
 
   /**
    * Initiate portfolio lightbox 
    */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
+  // const portfolioLightbox = GLightbox({
+  //   selector: '.portfolio-lightbox'
+  // });
 
   /**
    * Testimonials slider
@@ -226,7 +226,6 @@
       preloader.remove()
     });
   }
-
   /**
    * Initiate Pure Counter 
    */
